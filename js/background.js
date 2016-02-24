4d417a649c2c1ce627a22c7c703eb0b22193d32d
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2016-02-24 11:57:26 +0900

    Refactor background function

--- js/background.js
+++ js/background.js
@@ -1,25 +1,28 @@
-const BLOB_PATTERN = /https:\/\/github.com\/(.*)\/blob\/(.*)/;
+const BLOB_REGEXP = {
+  pattern: /(https:\/\/github.com)\/(.*)\/blob\/(.*)/,
+  replacement: "$1/$2/raw/$3"
+};
 
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
-  if(info.url.match(BLOB_PATTERN)) {
+  if(info.url.match(BLOB_REGEXP.pattern)) {
     chrome.pageAction.show(tabId);
   }
 });
 
 chrome.pageAction.onClicked.addListener(function(tab){
   chrome.tabs.getSelected(null, function(tab) {
-    var url = rawgitUrl(tab.url)
-    download(url);
+    var url = rawgitUrl(tab.url, BLOB_REGEXP)
+    var filename = url.match(".+/(.+?)([\?#;].*)?$")[1]
+    download(url, filename);
   })
 });
 
-function rawgitUrl(url) {
-  var contentUrl = url.replace(BLOB_PATTERN, "https://github.com/$1/raw/$2")
+function rawgitUrl(url, regexp) {
+  var contentUrl = url.replace(regexp.pattern, regexp.replacement)
   return contentUrl;
 }
 
-function download(url) {
-  var filename = url.match(".+/(.+?)([\?#;].*)?$")[1]
+function download(url, filename) {
   var dl = document.createElement('a');
   dl.href = url;
   dl.download = filename;
